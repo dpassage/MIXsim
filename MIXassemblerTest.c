535314e1f591387ca1fcb26525a68f9ab62c7f12
David Paschich <dpassage@flophouse.com>
2010-05-15 16:20:27 -0700

    array-based testcases for parser

--- MIXassemblerTest.c
+++ MIXassemblerTest.c
@@ -8,6 +8,7 @@
  */
 
 #include <check.h>
+#include <stdlib.h>
 
 #include "MIXassemblerTest.h"
 
@@ -31,12 +32,36 @@ START_TEST (test_parse_load)
 }
 END_TEST
 
+static char *testcases[][2] = {
+	{ "LDA 2000,2(0:3)", "+ 20 00 02 03 08" },
+	{ "LDA 2000,2(1:3)", "+ 20 00 02 11 08" },
+	{ NULL, NULL }
+};
+
+START_TEST (test_parser_cases)
+{
+	int i;
+	int parse_code;
+	mix_word *w = mix_word_create();
+	char *rendered_result;
+	
+	for (i = 0; testcases[i][0] != NULL; i++) {
+		parse_code = mix_assemble_line(testcases[i][0], w);
+		fail_unless(parse_code == 0, "Parser failed");
+		rendered_result = mix_word_tostring(w);
+		fail_unless(strcmp(rendered_result, testcases[i][1]) == 0, "Parser test %d failed: expected %s got %s", i, testcases[i][1], rendered_result);
+		free(rendered_result);
+	}
+}
+END_TEST
+
 Suite *mix_assembler_suite(void)
 {
 	Suite *s = suite_create("mix_assembler");
 	
 	TCase *tc_core = tcase_create("Core");
 	tcase_add_test (tc_core, test_parse_load);
+	tcase_add_test (tc_core, test_parser_cases);
 	suite_add_tcase (s, tc_core);
 	
 	return s;
