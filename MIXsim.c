1608704bdbe0b2d0b6caf392e541308f57fa423a
David Paschich <dpassage@flophouse.com>
2010-05-16 10:48:32 -0700

    load program test

--- MIXsim.c
+++ MIXsim.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "mix_assembler.h"
+#include "mix_machine.h"
 #include "mix_word.h"
 
 int main (int argc, const char * argv[]) {
-
+    mix_machine *m;
+    char readbuf[500];
+    int ip;
+    int i = 0;
+    int loc;
+	mix_word w;
     
     /* create new machine */
-
+    m = mix_machine_create();
+    
     /* read start line and set Ip */
+    if (fgets(readbuf, 500, stdin) != NULL) {
+        sscanf(readbuf, "START: %d\n", &ip);
+    }
+    
+    mix_machine_set_ip(m, ip);
+    
     /* for each remaining line */
-    /* read memory location */
-    /* set location in machine */
-	
-    /* print total locations set and exit */
-    
-    
-    char readbuf[500];
-	mix_word *w;
-	char *s;
-	
-	w = mix_word_create();
-	
-	/* for each line of input */
 	while (fgets(readbuf, 500, stdin) != NULL) {
-		mix_assemble_line(readbuf, w);
-		s = mix_word_tostring(w);
-		printf("%s\n", s);
-		free(s);
-	}
-	mix_word_destroy(w);
+        if (readbuf[4] != ':') {
+            continue;
+        }
+        readbuf[4] = '\0';
+        loc = atoi(readbuf);
+        mix_word_set(&w, &(readbuf[6]));
+        mix_machine_load_mem(m, &w, loc);
+        i++;
+    }
+    
+    printf("Locations loaded: %d\n", i);
+    printf("Starting instruction: %d\n", ip);
+    
     return 0;
 }
